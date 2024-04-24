859395d1dd6f3ed2a2bff0796783eb5dadd9c420
YilmazNL2005 <99069863@mydavinci.nl>
2024-04-24 22:12:04 +0200

    tussendoor_commit

--- pdf-generator/pdfsample.py
+++ pdf-generator/pdfsample.py
@@ -46,13 +46,39 @@ textLines = [
     f"Btw-nummer: 3362782",
     f"Kvk-nummer: 3336447"
 ]
+
+factuuradres = [
+    "Factuuradres",
+    "Sofia Philips",
+    "Overalstraat 123",
+    "1234 AB Beekstad",
+    "Verzendadres"
+]
+
+bedrijfsgegevens = [
+    "Klantenservice",
+    "123 - 4567890",
+    "321 - 4567890",
+    "info@pocketmates.nl",
+    "Bedrijfsgegevens",
+    "KVK: 3362782",
+    "KVK-nummer: 3336447"
+]
+
+kosten = [
+    "Subtotaal:",
+    "Verzending:",
+    "BTW(21%):"
+    "Totaal:"
+]
+
 zin = """Dit is een zin in de factuur.
         Hopelijk is het leuk
 """
 artikelomschrijving = "Artikelomschrijving"
 aantal = "Aantal"
 prijs = "Prijs"
-totaalprijs = "Totaalprijs"
+totaalprijs = "Totaal"
 
 # Image = 
 
@@ -85,21 +111,56 @@ pdf.drawString(235, 750, title)
 
 ##############################################
 # 3) Draw a line
-pdf.line(40, 540, 550, 540)
+pdf.line(40, 560, 550, 560)
+pdf.line(40, 510, 550, 510)
+pdf.line(40, 410, 550, 410)
+pdf.line(40, 310, 550, 310)
+pdf.line(40, 100, 550, 100)
 
 
 # 4) Benodigde Tekst wat er standaard in staat.
 
 pdf.setFont("Helvetica-Bold", 10)
-pdf.drawString(400, 720, textLines[0])
-pdf.drawString(400, 705, textLines[1])
-pdf.drawString(400, 690, textLines[2])
-pdf.drawString(400, 640, textLines[3])
-pdf.drawString(400, 580, textLines[4])
+pdf.drawString(400, 735, textLines[0])
+pdf.drawString(400, 720, textLines[1])
+pdf.drawString(400, 705, textLines[2])
+pdf.drawString(400, 655, textLines[3])
+pdf.drawString(400, 595, textLines[4])
+
+pdf.setFont("Helvetica-Bold", 12)
+
+# Kopjes
+pdf.drawString(450, 530, totaalprijs)
+pdf.drawString(350, 530, prijs)
+pdf.drawString(250, 530, aantal)
+pdf.drawString(80, 530, artikelomschrijving)
 
 
+pdf.setFont("Helvetica-Bold", 10)
+
+# Factuuradres
+pdf.drawString(80, 285, factuuradres[0])
+pdf.drawString(80, 265, factuuradres[1])
+pdf.drawString(80, 245, factuuradres[2])
+pdf.drawString(80, 225, factuuradres[3])
+
+# Verzendadres
+pdf.drawString(80, 185, factuuradres[4])
+pdf.drawString(80, 165, factuuradres[1])
+pdf.drawString(80, 145, factuuradres[2])
+pdf.drawString(80, 125, factuuradres[3])
 
 
+# Klantenservice
+pdf.drawString(80, 80, bedrijfsgegevens[0])
+pdf.drawString(80, 60, bedrijfsgegevens[1])
+pdf.drawString(80, 40, bedrijfsgegevens[2])
+pdf.drawString(80, 20, bedrijfsgegevens[3])
+
+# Bedrijfsgegevens
+pdf.drawString(450, 80, bedrijfsgegevens[4])
+pdf.drawString(450, 60, bedrijfsgegevens[5])
+pdf.drawString(450, 40, bedrijfsgegevens[6])
 
 ###############
 
